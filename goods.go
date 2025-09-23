c157d82c6f0c458cf8d1b4ba77ce315574e27173
jayyuan <2218773049@qq.com>
2025-09-24 01:21:37 +0800

    11

--- goods.go
+++ goods.go
@@ -3,6 +3,7 @@ package mysql_model_struct
 type Goods struct {
 	GoodsId    string  `gorm:"column:goods_id;primaryKey" json:"goodsId"` // 编号
 	Name       string  `gorm:"column:name" json:"name"`
+	CategoryId int64   `gorm:"column:category_id;index" json:"category_id"`    // 分类ID
 	GoodsType  string  `gorm:"column:goods_type;index" json:"goods_type"`      // 卡类型
 	Amount     float64 `gorm:"column:amount;type:decimal(18,2)" json:"amount"` // 面值
 	Icon       string  `gorm:"column:icon;type:varchar(255)" json:"icon"`      // 图标
