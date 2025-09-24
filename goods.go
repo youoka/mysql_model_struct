a476460d76fbaa67ab5d4d9c99d2d1cdfe63e9c4
jayyuan <2218773049@qq.com>
2025-09-24 23:43:09 +0800

    123

--- goods.go
+++ goods.go
@@ -3,10 +3,10 @@ package mysql_model_struct
 type Goods struct {
 	GoodsId    string  `gorm:"column:goods_id;primaryKey" json:"goodsId"` // 编号
 	Name       string  `gorm:"column:name" json:"name"`
-	CategoryId int64   `gorm:"column:category_id;index" json:"category_id"`    // 分类ID
-	GoodsType  string  `gorm:"column:goods_type;index" json:"goods_type"`      // 卡类型
-	Amount     float64 `gorm:"column:amount;type:decimal(18,2)" json:"amount"` // 面值
-	Icon       string  `gorm:"column:icon;type:varchar(255)" json:"icon"`      // 图标
+	Category   string  `gorm:"column:category;type:varchar(64),index" json:"category"` // 分类
+	GoodsType  string  `gorm:"column:goods_type;index" json:"goods_type"`              // 卡类型
+	Amount     float64 `gorm:"column:amount;type:decimal(18,2)" json:"amount"`         // 面值
+	Icon       string  `gorm:"column:icon;type:varchar(255)" json:"icon"`              // 图标
 	Status     int     `gorm:"column:status;type:int;default:0" json:"status"`
 	CreateTime int64   `gorm:"column:create_time;autoCreateTime" json:"createTime"`
 	UpdateTime int64   `gorm:"column:update_time;autoUpdateTime" json:"updateTime"`
