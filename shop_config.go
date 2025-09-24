a49a30505b0607a21e789234e6ecae30fa6bc6d8
jayyuan <2218773049@qq.com>
2025-09-24 23:31:37 +0800

    config

--- shop_config.go
+++ shop_config.go
@@ -2,7 +2,7 @@ package mysql_model_struct
 
 type ShopConfig struct {
 	ID         uint   `gorm:"primaryKey;autoIncrement" json:"id"`
-	ConfigType int    `gorm:"column:type;type:int" json:"configType"` // 1:系统配置 2:支付配置
+	ConfigType int    `gorm:"column:type;type:int" json:"configType"`
 	Values     string `gorm:"column:values;type:text" json:"values"`
 }
 
